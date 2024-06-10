292bde9c6c909bc8aea354ca7d8565b24f6801a3
galiev <ggvalion@gmail.com>
2024-06-10 19:19:15 +0300

    Routing

--- src/main/kotlin/dev/ise/plugins/Routing.kt
+++ src/main/kotlin/dev/ise/plugins/Routing.kt
@@ -1,9 +1,15 @@
 package dev.ise.plugins
 
+import dev.ise.routing.indexRoute
+import dev.ise.routing.v1.clothes
 import io.ktor.server.application.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-
+    routing {
+        indexRoute()
+        route("v1") {
+            clothes()
+        }
+    }
 }
--- src/main/kotlin/dev/ise/routing/IndexRoute.kt
+++ src/main/kotlin/dev/ise/routing/IndexRoute.kt
@@ -3,6 +3,6 @@ package dev.ise.routing
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 
-fun Route.indexRoutes() {
+fun Route.indexRoute() {
     staticResources("/", "")
 }
