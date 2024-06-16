58609cd09d666a78f6bc4c10c706c17ce41cf8e3
Vadim Ksenofontov <v.ksenofontov@innopolis.university>
2024-06-16 20:26:16 +0300

    Outfit DTO fixed

--- src/main/kotlin/dev/ise/dto/Outfit.kt
+++ src/main/kotlin/dev/ise/dto/Outfit.kt
@@ -8,5 +8,5 @@ data class Outfit(
     val name: String = "",
     val description: String = "",
     val image_id: Int = -1,
-    val clothes: MutableList<Cloth> = mutableListOf(),
+    val clothes: List<Int> = listOf()
 )
