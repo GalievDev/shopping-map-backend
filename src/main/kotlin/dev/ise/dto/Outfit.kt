55b29462b08c0e33d64abcca678d05ce09ca037f
Vadim Ksenofontov <v.ksenofontov@innopolis.university>
2024-06-16 20:07:37 +0300

    Outfit DTO fix

--- src/main/kotlin/dev/ise/dto/Outfit.kt
+++ src/main/kotlin/dev/ise/dto/Outfit.kt
@@ -3,10 +3,10 @@ package dev.ise.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Outfit (
+data class Outfit(
     val id: Int = -1,
     val name: String = "",
     val description: String = "",
     val image_id: Int = -1,
-    val clothes: List<Int> = listOf(),
+    val clothes: MutableList<Cloth> = mutableListOf(),
 )
