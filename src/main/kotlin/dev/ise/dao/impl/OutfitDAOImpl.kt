de2304a9da8bef187777decf84a77fbbaf47e949
Vadim Ksenofontov <v.ksenofontov@innopolis.university>
2024-06-17 23:02:08 +0300

    Outfit DAO interface & implementation fixed

--- src/main/kotlin/dev/ise/dao/impl/OutfitDAOImpl.kt
+++ src/main/kotlin/dev/ise/dao/impl/OutfitDAOImpl.kt
@@ -7,32 +7,34 @@ import dev.ise.mics.Database.update
 
 
 object OutfitDAOImpl : OutfitDAO {
-    override fun create(name: String, description: String, image: ByteArray): Int {
-        val id = ImageDAOImpl.create(name, image)
-        return if (id != null) {
-            return update("INSERT INTO outfit(name, description, image_id) VALUES('$name', '$description', '$id' )")
-        } else {
-            -1
+    override fun create(name: String, description: String, outfitClothIds: List<Int>, image: ByteArray): Int {
+        val imageId = ImageDAOImpl.create(name, image)
+        if (imageId == -1) return -1
+        val outfitId: Int = update("INSERT INTO outfits(name, description, image_id)" +
+                " VALUES('$name', '$description', '$imageId') RETURNING id")
+        if (outfitId == -1) return -1
+        var outfitKeysString = ""
+        for (i in 0..<outfitClothIds.size) {
+            outfitKeysString += "$outfitId, "
         }
+        outfitKeysString += outfitId
+        return update("INSERT INTO outfits_clothes ($outfitKeysString) VALUES" +
+                " (${outfitClothIds.joinToString(", ")})")
     }
 
     override fun deleteById(id: Int): Int = update("DELETE FROM outfits WHERE id IN($id)")
 
     override fun getAll(): List<Outfit> = mutableListOf<Outfit>().apply {
         query(
-            "SELECT outfits.id, outfits.name, outfits.description, outfits.image_id FROM" +
-                    " outfits, clothes, outfits_clothes, images WHERE outfits.image_id = images.id AND" +
-                    " outfits_clothes.cloth_id = clothes.id",
+            "SELECT id, name, description, image_id FROM outfits"
         ) { resultSet ->
-            val outfitId: Int = resultSet.getInt("id")
             while (resultSet.next()) {
                 add(
                     Outfit(
-                        outfitId,
+                        resultSet.getInt("id"),
                         resultSet.getString("name"),
                         resultSet.getString("description"),
                         resultSet.getInt("image_id"),
-                        getClothesIds(outfitId)
                     )
                 )
             }
@@ -41,26 +43,14 @@ object OutfitDAOImpl : OutfitDAO {
 
     override fun getById(id: Int): Outfit? {
         var returnVal: Outfit? = null
-        query("SELECT outfits.name, outfits.description, outfits.image_id FROM outfits WHERE outfits.id IN($id) LIMIT 1") { resultSet ->
+        query("SELECT name, description, image_id FROM outfits WHERE outfits.id IN($id) LIMIT 1") { resultSet ->
             returnVal = Outfit(
-                id, resultSet.getString("name"),
+                id,
+                resultSet.getString("name"),
                 resultSet.getString("description"),
                 resultSet.getInt("image_id"),
-                getClothesIds(id)
             )
         }
         return returnVal
-    }
-
-    private fun getClothesIds(id: Int): List<Int> {
-        return mutableListOf<Int>().apply {
-            query("SELECT outfits_clothes.cloth_id FROM outfits_clothes WHERE outfits_clothes.outfit_id IN($id)") { resultSet ->
-                while (resultSet.next()) {
-                    add(
-                        resultSet.getInt("cloth_id")
-                    )
-                }
-            }
-        }
     }
 }
