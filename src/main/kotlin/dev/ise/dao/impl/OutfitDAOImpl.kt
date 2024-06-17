ee192c4e1488e009b5600e4cacd655cc1c3dbbd0
galiev <ggvalion@gmail.com>
2024-06-17 18:15:53 +0300

    SQL Table fixed

--- src/main/kotlin/dev/ise/dao/impl/OutfitDAOImpl.kt
+++ src/main/kotlin/dev/ise/dao/impl/OutfitDAOImpl.kt
@@ -2,56 +2,59 @@ package dev.ise.dao.impl
 
 import dev.ise.dao.OutfitDAO
 import dev.ise.dto.Outfit
-import dev.ise.mics.Database
 import dev.ise.mics.Database.query
+import dev.ise.mics.Database.update
 
 
 object OutfitDAOImpl : OutfitDAO {
-    override fun create(name: String, description: String, image: ByteArray) : Int {
+    override fun create(name: String, description: String, image: ByteArray): Int {
         val id = ImageDAOImpl.create(name, image)
         return if (id != null) {
-            return Database.update("INSERT INTO outfit(name, description, image_id) VALUES('$name', '$description', '$id' )")
+            return update("INSERT INTO outfit(name, description, image_id) VALUES('$name', '$description', '$id' )")
         } else {
             -1
         }
     }
 
-    override fun deleteById(id: Int): Int = Database.update("DELETE FROM outfit WHERE id IN($id)")
+    override fun deleteById(id: Int): Int = update("DELETE FROM outfits WHERE id IN($id)")
 
-    override fun getAll(): List<Outfit> {
-        return mutableListOf<Outfit>().apply { query(
-            "SELECT outfit.id, outfit.name, outfit.description, outfit.image_id FROM" +
-                    " outfit, clothes, outfit_cloth, images WHERE outfit.image_id = images.id AND" +
-                    " outfit_cloth.cloth_id = clothes.id",
-        ) {resultSet ->
+    override fun getAll(): List<Outfit> = mutableListOf<Outfit>().apply {
+        query(
+            "SELECT outfits.id, outfits.name, outfits.description, outfits.image_id FROM" +
+                    " outfits, clothes, outfits_clothes, images WHERE outfits.image_id = images.id AND" +
+                    " outfits_clothes.cloth_id = clothes.id",
+        ) { resultSet ->
             val outfitId: Int = resultSet.getInt("id")
             while (resultSet.next()) {
-                add(Outfit(outfitId,
-                    resultSet.getString("name"),
-                    resultSet.getString("description"),
-                    resultSet.getInt("image_id"),
-                    getClothesId(outfitId)))
+                add(
+                    Outfit(
+                        outfitId,
+                        resultSet.getString("name"),
+                        resultSet.getString("description"),
+                        resultSet.getInt("image_id"),
+                        getClothesIds(outfitId)
+                    )
+                )
             }
-        }
         }
     }
 
-    override fun getById(id: Int) : Outfit? {
-        var returnVal : Outfit? = null
-        query("SELECT outfit.name, outfit.description, outfit.image_id FROM outfit WHERE outfit.id IN($id) LIMIT 1") {
-            resultSet ->
-            returnVal = Outfit(id, resultSet.getString("name"),
+    override fun getById(id: Int): Outfit? {
+        var returnVal: Outfit? = null
+        query("SELECT outfits.name, outfits.description, outfits.image_id FROM outfits WHERE outfits.id IN($id) LIMIT 1") { resultSet ->
+            returnVal = Outfit(
+                id, resultSet.getString("name"),
                 resultSet.getString("description"),
                 resultSet.getInt("image_id"),
-                getClothesId(id)
+                getClothesIds(id)
             )
         }
         return returnVal
     }
 
-    private fun getClothesId(id: Int) : List<Int> {
+    private fun getClothesIds(id: Int): List<Int> {
         return mutableListOf<Int>().apply {
-            query("SELECT outfit_cloth.cloth_id FROM outfit_cloth WHERE outfit_cloth.outfit_id = $id") { resultSet ->
+            query("SELECT outfits_clothes.cloth_id FROM outfits_clothes WHERE outfits_clothes.outfit_id IN($id)") { resultSet ->
                 while (resultSet.next()) {
                     add(
                         resultSet.getInt("cloth_id")
