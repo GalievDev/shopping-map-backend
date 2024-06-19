078bc4da093babade440cc01b31b55a81b5c2f1b
galiev <ggvalion@gmail.com>
2024-06-19 22:20:13 +0300

    Added outfit routing #3

--- src/main/kotlin/dev/ise/plugins/Routing.kt
+++ src/main/kotlin/dev/ise/plugins/Routing.kt
@@ -3,6 +3,7 @@ package dev.ise.plugins
 import dev.ise.routing.indexRoute
 import dev.ise.routing.v1.clothes
 import dev.ise.routing.v1.images
+import dev.ise.routing.v1.outfits
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
@@ -12,6 +13,7 @@ fun Application.configureRouting() {
         route("api/v1") {
             clothes()
             images()
+            outfits()
         }
     }
 }
