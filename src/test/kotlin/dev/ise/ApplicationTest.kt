943cfc6946e2a20170a33abcf43abfa8a3a3ac1d
galiev <ggvalion@gmail.com>
2024-07-06 10:34:26 +0300

    Unit Testing setup

--- src/test/kotlin/dev/ise/ApplicationTest.kt
+++ src/test/kotlin/dev/ise/ApplicationTest.kt
@@ -2,16 +2,35 @@ package dev.ise
 
 import dev.ise.shoppingmap.plugins.configureRouting
 import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
+import kotlinx.serialization.json.Json
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
+            install(ContentNegotiation) {
+                json(
+                    contentType = ContentType.Application.Json,
+                    json = Json {
+                        prettyPrint = true
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
             configureRouting()
         }
-        client.get("/").apply {
+        client.get("/api/v1/clothes").apply {
+            assertEquals(HttpStatusCode.OK, status)
         }
+/*        client.post("api/v1/clothes").apply {
+            assertEquals(HttpStatusCode.Created, status)
+        }*/
     }
 }
