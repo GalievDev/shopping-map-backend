35aeae659d44d01e3737b2332e80adf1cce2424b
galiev <ggvalion@gmail.com>
2024-07-11 12:28:03 +0300

    Image id displaying fixed

--- src/main/kotlin/dev/ise/shoppingmap/dao/impl/ClothDAOImpl.kt
+++ src/main/kotlin/dev/ise/shoppingmap/dao/impl/ClothDAOImpl.kt
@@ -15,14 +15,22 @@ object ClothDAOImpl: ClothDAO {
     override fun delete(id: Int): Int = update("DELETE FROM clothes WHERE id IN($id)")
 
     override fun getAll(): List<Cloth> = mutableListOf<Cloth>().apply {
-        query("SELECT *, images.id, images.name, images.bytes FROM clothes JOIN images ON clothes.image_id = images.id") { resultSet ->
+        query(
+        """
+        SELECT 
+            clothes.id AS cloth_id, clothes.name AS cloth_name, clothes.link, clothes.description, clothes.type,
+            images.id AS image_id, images.name AS image_name, encode(images.bytes, 'base64') AS image_bytes
+        FROM clothes 
+        JOIN images ON clothes.image_id = images.id
+        """
+        ) { resultSet ->
             while (resultSet.next()) {
                 add(
                     Cloth(
-                        resultSet.getInt("id"), resultSet.getString("name"),
+                        resultSet.getInt("cloth_id"), resultSet.getString("cloth_name"),
                         resultSet.getString("link"), resultSet.getString("description"), ClothType.valueOf(resultSet.getString("type")),
                         Image(
-                            resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("bytes")
+                            resultSet.getInt("image_id"), resultSet.getString("image_name"), resultSet.getString("image_bytes")
                         ).id
                     )
                 )
@@ -33,13 +41,23 @@ object ClothDAOImpl: ClothDAO {
     override fun getById(id: Int): Cloth? {
         var cloth: Cloth? = null
 
-        query("SELECT *, images.bytes FROM clothes JOIN images ON clothes.image_id = images.id WHERE clothes.id IN($id) LIMIT 1") { resultSet ->
+        query(
+        """
+        SELECT 
+            clothes.id AS cloth_id, clothes.name AS cloth_name, clothes.link, clothes.description, clothes.type,
+            images.id AS image_id, images.name AS image_name, encode(images.bytes, 'base64') AS image_bytes
+        FROM clothes 
+        JOIN images ON clothes.image_id = images.id 
+        WHERE clothes.id IN ($id) 
+        LIMIT 1
+        """
+        ) { resultSet ->
             while (resultSet.next()) {
                 cloth = Cloth(
-                    resultSet.getInt("id"), resultSet.getString("name"),
+                    resultSet.getInt("cloth_id"), resultSet.getString("cloth_name"),
                     resultSet.getString("link"), resultSet.getString("description"), ClothType.valueOf(resultSet.getString("type")),
                     Image(
-                        resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("bytes")
+                        resultSet.getInt("image_id"), resultSet.getString("image_name"), resultSet.getString("image_bytes")
                     ).id
                 )
             }
