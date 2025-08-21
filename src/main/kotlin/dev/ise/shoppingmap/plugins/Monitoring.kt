f26b58407bed2c3883e7d92d28bc11d80039178e
Vazir Vadirov <ggvalion@gmail.com>
2025-08-21 14:06:10 +0500

    Configured logging

--- src/main/kotlin/dev/ise/shoppingmap/plugins/Monitoring.kt
+++ src/main/kotlin/dev/ise/shoppingmap/plugins/Monitoring.kt
@@ -1,13 +1,21 @@
 package dev.ise.shoppingmap.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.request.*
 import org.slf4j.event.Level
 
-fun Application.configureMonitoring() {
+fun Application.configureLogging() {
     install(CallLogging) {
         level = Level.INFO
-        filter { call -> call.request.path().startsWith("/") }
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val userAgent = call.request.headers["User-Agent"]
+            val remoteHost = call.request.headers["X-Real-IP"] ?: call.request.origin.remoteHost
+            val path = call.request.path()
+            "[$remoteHost] $userAgent ($httpMethod)[$status] $path"
+        }
     }
 }
